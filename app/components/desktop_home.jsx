1e8682a7b007ad62a3db58237a6ab79090386c27
Sidak <magicalsidak@gmail.com>
2024-08-14 22:43:51 -0400

    up scalled image

--- app/components/desktop_home.jsx
+++ app/components/desktop_home.jsx
@@ -2,20 +2,20 @@ import Image from "next/image";
 import styles from "./desktop_home.module.css";
 import localFont from "next/font/local";
 
-export default function DesktopHome() {
-  const myFont = localFont({
-    src: "./fonts/cyber-reg-font.woff2",
-    display: "swap",
-  });
+const myFont = localFont({
+  src: ".././fonts/cyber-reg-font.woff2",
+  display: "swap",
+});
 
+export default function DesktopHome() {
   return (
     <section className="w-full h-screen bg-[#0A0F13]">
       <div className="flex items-center justify-center w-full h-screen">
-        <div className="flex items-center justify-evenly">
+        <div className="flex items-center justify-evenly w-full">
           <Image src="/home-profile.png" height={412} width={413} />
-          <div>
+          <div className="">
             <div className={`${styles.myName}`}>My Name </div>
-            <div className={`${styles.isSidak} ${myFont.className}`}>
+            <div className={`${myFont.className} ${styles.isSidak} mt-9 ml-32`}>
               Is SidaK
             </div>
           </div>
