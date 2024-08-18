5870659c93e30c4d494ba5a6f963863a7461ef9e
Sidak <magicalsidak@gmail.com>
2024-08-17 21:26:48 -0400

    going to pivot

--- app/components/home/desktop_home.jsx
+++ app/components/home/desktop_home.jsx
@@ -4,61 +4,88 @@ import styles from "./desktop_home.module.css";
 import localFont from "next/font/local";
 import { useGlitch } from "react-powerglitch";
 import { motion, useScroll } from "framer-motion";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const myFont = localFont({
   src: "../.././fonts/cyber-reg-font.woff2",
   display: "swap",
 });
 
-export default function DesktopHome({ active, setActive }) {
+// function DesktopHome({ active, setActive }) {
+//   const glitch = useGlitch();
+//   const ref = useRef(null);
+//   const { scrollYProgress } = useScroll({
+//     target: ref,
+//     offset: ["end end", "start start"],
+//   });
+
+//   useEffect(() => {
+//     const intervalId = setInterval(() => {
+//       console.log(10, scrollYProgress.get());
+//     }, 10);
+
+//     return () => clearInterval(intervalId); // Cleanup interval on component unmount
+//   }, [scrollYProgress]);
+
+//   return (
+//     <section id="home">
+//       <div ref={ref}>
+//         <div className="w-full h-screen bg-[#0A0F13]">
+//           <div className="flex items-center justify-center w-full h-screen mb-[200px]">
+//             <div className="flex items-center justify-evenly w-full">
+//               <div ref={glitch.ref}>
+//                 <Image src="/home-profile.png" height={412} width={413} />
+//               </div>
+//               <div className="">
+//                 <div className={`${styles.myName}`}>My Name </div>
+//                 <div
+//                   className={`${myFont.className} ${styles.isSidak} mt-9 ml-32`}
+//                 >
+//                   Is SidaK
+//                 </div>
+//               </div>
+//             </div>
+//             <h1>{scrollYProgress.get()}</h1>
+//           </div>
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
+
+function Item() {
   const glitch = useGlitch();
   const ref = useRef(null);
   const { scrollYProgress } = useScroll({
     target: ref,
     offset: ["end end", "start start"],
   });
+  const [scrollPos, setScrollPos] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
       console.log(10, scrollYProgress.get());
+      setScrollPos(scrollYProgress.get());
+      // console.log(7, scrollYProgress.get());
     }, 10);
 
     return () => clearInterval(intervalId); // Cleanup interval on component unmount
   }, [scrollYProgress]);
 
   return (
-    <section id="home" setActiveSection={setActive}>
+    <section id="home">
       <div ref={ref}>
-        <div className="w-full h-screen bg-[#0A0F13]">
-          <div className="flex items-center justify-center w-full h-screen mb-[200px]">
-            <div className="flex items-center justify-evenly w-full">
-              <div ref={glitch.ref}>
-                <Image src="/home-profile.png" height={412} width={413} />
-              </div>
-              <div className="">
-                <div className={`${styles.myName}`}>My Name </div>
-                <div
-                  className={`${myFont.className} ${styles.isSidak} mt-9 ml-32`}
-                >
-                  Is SidaK
-                </div>
-              </div>
-            </div>
-            <svg id="progress" width="75" height="75" viewBox="0 0 100 100">
-              <circle cx="50" cy="50" r="30" pathLength="1" className="bg" />
-              <motion.circle
-                cx="50"
-                cy="50"
-                r="30"
-                pathLength="1"
-                className="indicator"
-                style={{ pathLength: scrollYProgress }}
-              />
-            </svg>
-          </div>
-        </div>
+        <div className="h-screen"></div>
+        <h1>{scrollYProgress.get()}</h1>
       </div>
     </section>
+  );
+}
+
+export default function DesktopHome() {
+  return (
+    <>
+      <Item />
+    </>
   );
 }
--- app/components/test/scroll.jsx
+++ app/components/test/scroll.jsx
@@ -1,5 +1,5 @@
-import "./styles.css";
-import { useRef, useEffect } from "react";
+// import "./styles.css";
+import { useRef, useEffect, useState } from "react";
 import { motion, useScroll } from "framer-motion";
 
 function Item() {
@@ -8,10 +8,12 @@ function Item() {
     target: ref,
     offset: ["end end", "start start"],
   });
+  const [scrollPos, setScrollPos] = useState(0);
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      console.log(7, scrollYProgress.get());
+      setScrollPos(scrollYProgress.get());
+      // console.log(7, scrollYProgress.get());
     }, 10);
 
     return () => clearInterval(intervalId); // Cleanup interval on component unmount
@@ -20,19 +22,7 @@ function Item() {
   return (
     <section>
       <div ref={ref}>
-        <figure className="progress">
-          <svg id="progress" width="75" height="75" viewBox="0 0 100 100">
-            <circle cx="50" cy="50" r="30" pathLength="1" className="bg" />
-            <motion.circle
-              cx="50"
-              cy="50"
-              r="30"
-              pathLength="1"
-              className="indicator"
-              style={{ pathLength: scrollYProgress }}
-            />
-          </svg>
-        </figure>
+        <h1>{scrollYProgress.get()}</h1>
       </div>
     </section>
   );
